d569109d40a3e55256e212d06a56df49c69f9201
Your Name <you@example.com>
2023-10-03 08:58:54 -0300

    Módulo masa creado

--- masa.py
+++ masa.py
@@ -1,1 +1,24 @@
 """2) Módulo masa.py: Este módulo debe contener funciones para convertir entre diferentes unidades de masa, como kilogramos, libras y onzas.(0.5 puntos) """
+def kilogramos_a_gramos(kg):
+    gr = gr = kg * 1000
+    return gr
+
+def kilogramos_a_toneladas(kg):
+    t = kg / 1000
+    return t
+
+def gramos_a_kilogramos(gr):
+    kg = gr / 1000
+    return kg
+
+def gramos_a_toneladas(gr):
+    t = gr / 1000000
+    return t
+
+def toneladas_a_kilogramos(t):
+    kg = t * 1000
+    return kg
+
+def toneladas_a_gramos(t):
+    g = t * 1000000
+    return g
